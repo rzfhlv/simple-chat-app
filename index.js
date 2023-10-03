1613eeea2e8a10af291ec4a173b95143ebaa72a0
rzfhlv <rzfhlv.me@gmail.com>
2023-10-03 20:59:45 +0700

    simple chat socket.io

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(new URL('./index.html', import.meta.url).pathname);
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log(`server running at http://localhost:3000`);
+});
